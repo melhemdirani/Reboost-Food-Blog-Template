141dcf09a1a46b3b97a02fda06f3c0fde9b34542
Naim Morshed <morshed.naim13@gmail.com>
2022-03-30 01:59:04 -0700

    added post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,41 @@
-import "./Post.styles.scss";
+import React from 'react'
+import img1 from '../../assets/images/blogf1.jpg'
+import img2 from '../../assets/images/blogf2.jpg'
 
-export default function Post() {
-    return (<></>);
+import './Blog.styles.scss';
+import { paragraph, best, eggsWithHam, date } from './fakeData';
+
+const Post = () => {
+    return (
+        <div>
+            <div className='Post_Container'>
+                <div className="row">
+                    <div className="image-container">
+                        <img src={img1} alt="Friends egg with ham" />
+                    </div>
+                    <div className="details">
+                        <h5>healthy</h5>
+                        {eggsWithHam}
+                        {date}
+                        {paragraph}
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="details">
+                        {best}
+                        <h3>Steak with boiled vegetables</h3>
+                        {date}
+                        {paragraph}
+                        <button>Read more</button>
+                    </div>
+                    <div className="image-container">
+                        <img src={img2} alt="Steak with boiled vegetables" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default Blog;                 
