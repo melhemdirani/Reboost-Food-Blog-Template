0335ebf5ba869eaa5344970b68fff9364d8e127f
Naim Morshed <morshed.naim13@gmail.com>
2022-03-30 02:19:41 -0700

    navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,56 @@
 import React from 'react';
 import { ImTwitter, ImDribbble, ImBehance, ImLinkedin2, ImFacebook, ImSearch, ImMenu } from 'react-icons/im'
-import { BsPinterest } from 'react-icons/bs'
-
+import { BsPinterest, FaSearch } from 'react-icons/bs';
 import './Navbar.styles.scss';
-import navImg from '../../assets/images/nav-img.jpg'
 
 export default function Navbar() {
     return (
         <div className='Navbar_Container'>
-            <nav>
-                <div>
-                    <div className='navbar_img'>
-                        <img src={navImg} alt='navbarImage' />
-                        <div className='navbar-top'>
-                            <div className='navbar-icons'>
-                                <BsPinterest id="pinterest" />
-                                {/* mr : margin right */}
-                                <ImFacebook id="mr" />
-                                <ImTwitter id="mr" />
-                                <ImDribbble id="mr" />
-                                <ImBehance id="mr" />
-                                <ImLinkedin2 id="mr" />
-                            </div>
-                            <div className='navbar-search'>
-                                <input type="text" name='search' placeholder='Search' size='40' />
-                                <ImSearch id="search" />
-                            </div>
+            <div className='header'>
+                <div className='header-top'>
+                    <div className='header-icons'>
+                        <a href="/"><BsPinterest /></a>
+                        <a href="/"><ImFacebook /></a>
+                        <a href="/"><ImTwitter /></a>
+                        <a href="/"><ImDribbble /></a>
+                        <a href="/"><ImBehance /></a>
+                        <a href="/"><ImLinkedin2 /></a>
+                    </div>
+                    <div className='navbar-search'>
+                        <div className='search-bar'>
+                            <input type="text" name='search' placeholder='Search' size="40" />
+                            <button id="search"><ImSearch /></button>
                         </div>
                     </div>
-                    <div className='navbar-header-text'>
-                        <p className='navbar-header-text-p1'>Bueno</p>
-                        <p className='navbar-header-text-p2'>Food Blog</p>
-                    </div>
-                    <div>
-                        <ul>
+                </div>
+            </div>
+            <nav>
+                <div className='navbar-header-text'>
+                    <p className='navbar-header-text-p1'>Bueno</p>
+                    <p className='navbar-header-text-p2'>Food Blog</p>
+                </div>
+                <div className='navbar'>
+                    <ul>
+                        <div>
                             <li id="menu" ><a href='#none'><ImMenu /></a></li>
+                        </div>
+                        <div>
                             <li id="home" ><a href="#home">Home</a></li>
                             <li><a href="#aboutus">About Us</a></li>
-                            <li><a href="#pages">Pages</a></li>
+                            <li><a href="#pages">Pages</a>
+                                <div className="dropdown-menu">
+                                    <ul>
+                                        <li><a href="/">Page 1</a></li>
+                                        <li><a href="/">Page 2</a></li>
+                                    </ul>
+                                </div>
+                            </li>
                             <li><a href="#recipes">Recipes</a></li>
                             <li><a href="#blog">Blog</a></li>
                             <li><a href="#contact">Contact</a></li>
-                            <li id="login" ><a href="#Login/Register" className='navbar-link-last'>Login/Register</a></li>
-                        </ul>
-                    </div>
+                            <li id="login" ><a href="#Login/Register">Login/Register</a></li>
+                        </div>
+                    </ul>
                 </div>
             </nav>
         </div>
