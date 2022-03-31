1a109f2896a9d3b806267091a70cbfb3ac2ef4ac
Naim Morshed <morshed.naim13@gmail.com>
2022-03-30 22:40:43 -0700

    gallery imported

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { data } from './fakeData';
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+import './Gallery.scss';
+
+const Gallery = () => {
+    const [model, setModel] = useState(false);
+    const [tempimgSrc, setTempImgSrc] = useState('');
+
+    const getImg = (imgSrc) => {
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    }
+
+    return (
+        <div className='Gallery_Container'>
+            <div className={model ? "model open" : "model"}>
+                <img src={tempimgSrc} alt="" />
+                <AiOutlineCloseCircle onClick={() => setModel(false)} />
+            </div>
+
+            <div className="gallery">
+                {
+                    data.map((item, index) => {
+                        return (
+                            <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
+                                <img src={item.imgSrc} alt="" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
---
+++ src/components/Gallery/fakeData.js
@@ -0,0 +1,53 @@
+import blogf1 from '../../assets/images/blogf1.jpg';
+import blogf2 from '../../assets/images/blogf2.jpg';
+import blogf3 from '../../assets/images/blogf3.jpg';
+import blogf4 from '../../assets/images/blogf4.jpg';
+import blogf5 from '../../assets/images/blogf5.jpg';
+import blogf6 from '../../assets/images/blogf6.jpg';
+import blogf7 from '../../assets/images/blogf7.jpg';
+import blogf8 from '../../assets/images/blogf8.jpg';
+import blogf9 from '../../assets/images/blogf9.jpg';
+import blogf10 from '../../assets/images/blogf10.jpg';
+
+export const data = [
+    { 
+        id: 1,
+        imgSrc: blogf1,
+    },
+    {
+        id: 2,
+        imgSrc: blogf2,
+    },
+    {
+        id: 3,
+        imgSrc: blogf3,
+    },
+    {
+        id: 4,
+        imgSrc: blogf4,
+    },
+    {
+        id: 5,
+        imgSrc: blogf5,
+    },
+    {
+        id: 6,
+        imgSrc: blogf6,
+    },
+    {
+        id: 7,
+        imgSrc: blogf7,
+    },
+    {
+        id: 8,
+        imgSrc: blogf8,
+    },
+    {
+        id: 9,
+        imgSrc: blogf9,
+    },
+    {
+        id: 10,
+        imgSrc: blogf10,
+    },
+]
