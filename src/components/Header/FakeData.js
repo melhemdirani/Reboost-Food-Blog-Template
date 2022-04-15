f7799a40163fb257b016d99dd68648a02001f835
Naim Morshed <morshed.naim13@gmail.com>
2022-04-15 15:32:49 +0600

    header making resposive

---
+++ src/components/Header/FakeData.js
@@ -0,0 +1,22 @@
+import healthyFood from '../../assets/images/f1.jpg';
+import organicCuisine from '../../assets/images/f2.jpg';
+import vegetarianFood from '../../assets/images/f3.jpg'
+
+
+export const Data = [
+    {
+        title: 'The best',
+        img: healthyFood,
+        text: 'Healthy Food'
+    },
+    {
+        title: 'The best',
+        img: organicCuisine,
+        text: 'Organic Cuisine'
+    },
+    {
+        title: 'The best',
+        img: vegetarianFood,
+        text: 'Vegeterian Food'
+    },
+]
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,24 @@
-import React from 'react';
+import { Data } from './FakeData';
 import './Header.styles.scss';
 
-import healthyFood from '../../assets/images/f1.jpg';
-import organicCuisine from '../../assets/images/f2.jpg';
-import vegetarianFood from '../../assets/images/f3.jpg'
 
 export default function Header() {
-    const toggleImage = () => {
-
-    }
-
     return (
-        <div>
+        <center>
             <div className='Header_Container'>
-
-                <div className='Item'>
-                    <img onMouseOver={toggleImage} src={healthyFood} alt="healthy food" />
-                    <div className='absolute'>
-                        <h6>The best </h6>
-                        <p>Healthy Food </p>
-                    </div>
-                </div>
-
-                <div className='Item'>
-                    <img onMouseOver={toggleImage} src={organicCuisine} alt="organic cuisine" />
-                    <div className='absolute'>
-                        <h6>The best </h6>
-                        <p>Organic Cuisine </p>
-                    </div>
-                </div>
-
-                <div className='Item'>
-                    <img onMouseOver={toggleImage} src={vegetarianFood} alt="vegetarian food" />
-                    <div className='absolute'>
-                        <h6>The best </h6>
-                        <p>Vegetarian Food </p>
-                    </div>
-                </div>
-
+                {
+                    Data.map((data, index) => (
+                        <main>
+                            <img src={data.img} alt={index} />
+                            <div className='absolute'>
+                                <h6>{data.title}</h6>
+                                <p>{data.text}</p>
+                            </div>
+                        </main>
+                    ))
+                }
             </div>
-        </div>
+        </center>
     )
 }
 
