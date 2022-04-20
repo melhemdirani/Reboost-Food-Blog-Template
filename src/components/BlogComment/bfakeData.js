917368c2b61aa31496679e97fa0830af88f3ceea
Naim Morshed <morshed.naim13@gmail.com>
2022-04-20 23:53:32 +0600

    comment

---
+++ src/components/BlogComment/bfakeData.js
@@ -0,0 +1,22 @@
+export const bfakeData = [
+    {
+        name: 'Jane Smith',
+        date: 'July 11, 2018',
+        reply: 'Reply',
+        passage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit bibendum pharetra non vitae lectus. Mauris libero felis, dapibus a ultrices sed, commodo.'
+    },
+
+    {
+        name: 'Christian Williams',
+        date: 'April 15, 2018',
+        reply: 'Reply',
+        passage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit bibendum pharetra non vitae lectus. Mauris libero felis, dapibus a ultrices sed, commodo.'
+    },
+
+    {
+        name: 'Cris Williams',
+        date: 'July 11, 2018',
+        reply: 'Reply',
+        passage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tristique justo id elit bibendum pharetra non vitae lectus. Mauris libero felis, dapibus a ultrices.'
+    }
+]
