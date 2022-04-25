d6cbaa449f00321698f2d76dceb9ff81172dbdda
Sabit-Hasan <hayshin.sabit@gmail.com>
2022-04-25 22:36:30 +0600

    Top Scroll Done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 import "./App.css";
 
 import HomePage from "./pages/HomePage/HomePage";
 import Blog from "./pages/Blog/Blog";
 import Preloader from './components/Preloader/Preloader';
+import Contact from "./pages/Contact/Contact";
 
 
 export default function App() {
@@ -24,9 +26,11 @@ export default function App() {
             {
                 // isLoading === true ? <Preloader /> :
                 <BrowserRouter>
+                    <ScrollToTop smooth style={{backgroundColor: 'black', borderRadius: '50%'}} color="white" />
                     <Routes>
                         <Route path="/" element={<HomePage />} />
                         <Route path="/blog" element={<Blog />} />
+                        <Route path="/contact" element={<Contact />} />
                     </Routes>
                 </BrowserRouter>
             }
